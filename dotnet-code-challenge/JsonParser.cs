1d67bf22760e3e98c0eef24b1f23e58cd110250a
anilbas14 <anilbas@yahoo.com>
2019-11-24 15:21:21 +1100

    JSON parsing class implemented (tests pass)

--- dotnet-code-challenge/JsonParser.cs
+++ dotnet-code-challenge/JsonParser.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Collections.Generic;
 
 namespace dotnet_code_challenge
@@ -7,8 +9,30 @@ namespace dotnet_code_challenge
     {
         public Dictionary<string, decimal> Parse(string json)
         {
-		    // parse json, return dictionary (horse, price)
-			throw new NotImplementedException("Creating tests first.");
+			// parse json, return dictionary (horse, price)
+			var horses = new Dictionary<string, decimal>(){};
+			string name;
+			decimal price;
+			try 
+			{
+				JsonDocument document = JsonDocument.Parse(json);
+				foreach (JsonElement element in document.RootElement.GetProperty("RawData").GetProperty("Markets").EnumerateArray())
+				{
+					foreach (JsonElement element1 in element.GetProperty("Selections").EnumerateArray())
+					{
+						name = element1.GetProperty("Tags").GetProperty("name").ToString();
+						price = element1.GetProperty("Price").GetDecimal();	
+						horses.Add (name, price);
+					}
+
+				}
+				return horses;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				return new Dictionary<string, decimal>(){};				
+			} 
         }
     }
 }
