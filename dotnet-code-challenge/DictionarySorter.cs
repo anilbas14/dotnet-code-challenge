6d75470eb210a79fd591cd35d088b50c163ae943
anilbas14 <anilbas@yahoo.com>
2019-11-24 16:06:53 +1100

    Dictionary sorter class implemented (tests pass)

--- dotnet-code-challenge/DictionarySorter.cs
+++ dotnet-code-challenge/DictionarySorter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace dotnet_code_challenge
 {
@@ -7,7 +8,11 @@ namespace dotnet_code_challenge
     {
         public Dictionary<string, decimal> Sort(Dictionary<string, decimal> input)
         {
-			throw new NotImplementedException("Creating tests first");
+			var sorted = from pair in input
+                    orderby pair.Value ascending
+                    select pair;
+					
+			return sorted.ToDictionary(pair => pair.Key, pair => pair.Value);
         }
     }
 }
