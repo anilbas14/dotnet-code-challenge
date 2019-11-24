d1ba096223ec206485c4a682e68eddc43b3f5f06
anilbas14 <anilbas@yahoo.com>
2019-11-24 15:12:12 +1100

    JSON parsing tests and class stub (tests fail)

--- dotnet-code-challenge.Test/UnitTest1.cs
+++ dotnet-code-challenge.Test/UnitTest1.cs
@@ -1,14 +1,58 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
 
 namespace dotnet_code_challenge.Test
 {
-    public class UnitTest1
+    public class JsonParser_ParseShould
     {
+		private readonly JsonParser _jp;
+		
+		string json = 
+		"{\n"+
+		"  \"RawData\": {\n"+
+		"    \"Markets\": [\n"+
+		"     {\n"+
+		"        \"Selections\": [\n"+
+		"          {\n"+
+		"            \"Price\": 8.4,\n"+
+		"            \"Tags\": {\n"+
+		"             \"name\": \"Horse1\"\n"+
+		"            }\n"+
+		"          },\n"+
+		"          {\n"+
+		"            \"Price\": 7.6,\n"+
+		"            \"Tags\": {\n"+
+		"              \"name\": \"Horse2\"\n"+
+		"            }\n"+
+		"          }\n"+
+		"        ]\n"+
+		"	  }\n"+
+		"	]\n"+
+		"  }\n"+
+		"}";
+		public JsonParser_ParseShould()
+		{
+			_jp = new JsonParser();
+		}
         [Fact]
-        public void Test1()
+        public void Parse_EmptyStringInput_ReturnsEmptyDictionary()
         {
-
+			var result = _jp.Parse("");			
+			Assert.Equal(new Dictionary<string, decimal>(){}, result);
+			
         }
+		
+		[Fact]
+        public void Parse_Json_ReturnsCorrectDictionary()
+        {
+			var result = _jp.Parse(json);
+            var correctresult = new Dictionary<string, decimal>(){};
+			correctresult.Add("Horse1", (decimal)8.4);	
+			correctresult.Add("Horse2", (decimal)7.6);		
+			Assert.Equal(correctresult, result);
+			
+        }
+		
     }
 }
---
+++ dotnet-code-challenge/JsonParser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace dotnet_code_challenge
+{
+    public class JsonParser
+    {
+        public Dictionary<string, decimal> Parse(string json)
+        {
+		    // parse json, return dictionary (horse, price)
+			throw new NotImplementedException("Creating tests first.");
+        }
+    }
+}
